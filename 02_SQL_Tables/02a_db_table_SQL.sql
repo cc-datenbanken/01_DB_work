05830a3d19d0de1097584d0226d296d477735ec3
Cullain <cullain@gmx.de>
2023-07-04 09:37:45 +0000

    Create und Drop

---
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -0,0 +1,17 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 */
+# Zeilenkommentar 1
+-- Zeilenkommentar 2
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+DROP DATABASE IF EXISTS boo;
+
+SHOW DATABASES;

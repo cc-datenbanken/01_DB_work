6d9745bf9e5447d093caa3c64155eb60662e7cb1
Cullain <cullain@gmx.de>
2023-07-05 07:04:31 +0000

    Code wiederherstellen

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -1,20 +1,17 @@
 /* ------  Strukturen ----- */
 
-<<<<<<< HEAD
-/*      
-    NULL wird nicht mehr zugelassen
-=======
+
+   
+#NULL wird nicht mehr zugelassen
+
 /* 	 
 	NULL wird nicht mehr zugelassen
->>>>>>> 7089127d896a33e7015a94437d6b8387605b11f2
+
     DEFAULT-Werte eintragen
 */
 
-
-<<<<<<< HEAD
-=======
 /* DB boo löschen, falls vorhanden*/
->>>>>>> 7089127d896a33e7015a94437d6b8387605b11f2
+
 DROP DATABASE IF EXISTS boo;
 
 /* DB boo anlegen, falls noch nicht vorhanden*/
@@ -23,12 +20,11 @@ CREATE DATABASE IF NOT EXISTS boo;
 /* DB auswählen */
 USE boo;
 
-<<<<<<< HEAD
-CREATE TABLE test
+/*CREATE TABLE test
 (
     name VARCHAR(20),
     age INT    
-=======
+);*/
 /* Tabelle test löschen, falls vorhanden*/
 DROP TABLE IF EXISTS test;
 
@@ -37,15 +33,11 @@ CREATE TABLE IF NOT EXISTS test
 (
 	name VARCHAR(20) NOT NULL DEFAULT "TBA",
 	age INT	NOT NULL DEFAULT 0
->>>>>>> 7089127d896a33e7015a94437d6b8387605b11f2
+
 );
 
 /* Alle Tabellen in der DB anzeigen */
 SHOW TABLES;
-
-<<<<<<< HEAD
-/*Tabelle anlegen*/
-DESCRIBE test;
 
 /* ----- Daten ------- */
 INSERT INTO test(name,age) 
@@ -60,7 +52,7 @@ VALUES ();
 /* ---- Inhalte der Tabelle anzeigen ---- */
 
 SELECT * FROM test;
-=======
+
 /* Struktur der Tabelle anzeigen */
 DESCRIBE test;
 
@@ -76,5 +68,3 @@ INSERT INTO test(age,name) VALUES (35,"Alonzo");
 
 /* ---- Inhalte der Tabelle anzeigen (Ergebnistab.)---- */
 SELECT * FROM test;
- 
->>>>>>> 7089127d896a33e7015a94437d6b8387605b11f2
